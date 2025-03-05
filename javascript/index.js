e93d77c92a92900ab3c946d72df55981307ba5dc
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-05 23:34:42 +0600

    SuperSimpleDev-Start

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,7 @@
-console.log("Item (" + (1 + 1) + ") : $" + (2095 + 799) / 100);
+// console.log("Item (" + (1 + 1) + ") : $" + (2095 + 799) / 100);
+
+// console.log("I'm learning JavaScript");
+
+// console.log("I'm learning \n JavaScript");
+
+console.log(`Item (${1 + 1}) : $${(2095 + 799) / 100} `);
