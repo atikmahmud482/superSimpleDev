2a7468a3a2e97bbeccd53a00a73d8e546cf1bdec
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-14 23:34:28 +0600

    loops

--- javascript/scripts/11-todo-list.js
+++ javascript/scripts/11-todo-list.js
@@ -1,5 +1,13 @@
-const todoList = [];
+const todoList = ["make dinner", "wash dishes"];
 
+let todoListHTML = "";
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
