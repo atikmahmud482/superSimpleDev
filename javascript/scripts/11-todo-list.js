c9839723586f28cb3db6ae7894ab11bb0ba5fb21
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-15 23:59:14 +0600

    todo list styling

--- javascript/scripts/11-todo-list.js
+++ javascript/scripts/11-todo-list.js
@@ -19,10 +19,10 @@ function renderTodoList() {
     // const name = todoObject.name;
     // const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
-    const html = `<p>${name} ${dueDate} <button onclick ='
+    const html = `<div>${name} </div>  <div>${dueDate}</div>  <button onclick ='
     todoList.splice(${i}, 1);
     renderTodoList();
-    '>Delete</button> </p>`;
+    '>Delete</button>`;
     todoListHTML += html;
   }
 
