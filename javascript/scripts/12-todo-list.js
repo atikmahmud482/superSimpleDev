07136b6268b5da8ef98b12aa2bc18544ee6632b7
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-20 17:38:21 +0600

    addevenlisner added todolist

--- javascript/scripts/12-todo-list.js
+++ javascript/scripts/12-todo-list.js
@@ -16,16 +16,20 @@ function renderTodoList() {
 
   todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
-    const html = `<div>${name} </div>  <div>${dueDate}</div>  <button onclick ="
-      todoList.splice(${index}, 1);
-      renderTodoList();
-      " class = "delete-todo-button js-delete-todo-button">Delete</button>`;
+    const html = `<div>${name} </div>  <div>${dueDate}</div>  <button  class = "delete-todo-button js-delete-todo-button">Delete</button>`;
     todoListHTML += html;
   });
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 
-  document.querySelectorAll(".js-delete-todo-button");
+  document
+    .querySelectorAll(".js-delete-todo-button")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
 
 document.querySelector(".js-add-todo-button").addEventListener("click", () => {
