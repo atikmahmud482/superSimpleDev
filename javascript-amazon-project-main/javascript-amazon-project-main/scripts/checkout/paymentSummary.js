d1df36a63edc43a7029fe4052457f6d2334f82a8
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-27 23:31:27 +0600

    updating every value

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formateCurrency } from "../../utils/money.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -16,4 +17,48 @@ export function renderPaymentSummary() {
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
   const texCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + texCents;
+
+  const paymentSummaryHTML = `
+  <div class="payment-summary-title">Order Summary</div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$${formateCurrency(
+              productPriceCents
+            )}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${formateCurrency(
+              shippingPriceCents
+            )}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${formateCurrency(
+              totalBeforeTaxCents
+            )}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${formateCurrency(
+              texCents
+            )}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${formateCurrency(
+              totalCents
+            )}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+          
+          `;
 }
