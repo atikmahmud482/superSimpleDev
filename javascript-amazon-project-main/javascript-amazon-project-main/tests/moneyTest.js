828cfb286a042ac310670ea1941ec1d0c08de73b
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-28 22:27:24 +0600

    moneyTest.js

--- javascript-amazon-project-main/javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests/moneyTest.js
@@ -1,1 +1,7 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formateCurrency } from "../scripts/utils/money.js";
+
+if (formateCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
