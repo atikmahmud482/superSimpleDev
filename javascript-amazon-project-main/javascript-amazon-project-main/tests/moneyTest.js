1fc627348022bd75750ff196deac6f4fa27b884b
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-28 22:29:19 +0600

    moneyTest.js live check on live page

--- javascript-amazon-project-main/javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests/moneyTest.js
@@ -5,3 +5,9 @@ if (formateCurrency(2095) === "20.95") {
 } else {
   console.log("failed");
 }
+
+if (formateCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
