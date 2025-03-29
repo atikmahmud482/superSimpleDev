79eced7cf91cd63bd93868eb7fbd53f89f53422c
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-29 22:31:13 +0600

    jasmon code

--- javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/moneyTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -4,4 +4,8 @@ describe("test suite: formateCurrency", () => {
   it("converts cents into dollars", () => {
     expect(formateCurrency(2095)).toEqual("20.95");
   });
+
+  it("works with 0", () => {
+    expect(formateCurrency(0)).toEqual("0.00");
+  });
 });
