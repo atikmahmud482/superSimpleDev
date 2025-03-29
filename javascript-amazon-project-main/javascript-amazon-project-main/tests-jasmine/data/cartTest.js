8a1cf73bb0140c9e2bdca938fecfbd4488b99d60
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-29 23:10:22 +0600

    jasmon file to add another section

--- javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -2,12 +2,13 @@ import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe("test suite: addToCart", () => {
   it("adds an exiting product to the cart", () => {
+    spyOn(localStorage, "setItem");
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([]);
     });
-    console.log(localStorage.getItem("cart"));
     loadFromStorage();
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 });
