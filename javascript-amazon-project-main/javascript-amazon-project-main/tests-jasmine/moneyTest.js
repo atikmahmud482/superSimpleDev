09744850645794881fd264f041f57d36dfbca357
atikmahmud482 <atikmahmud482@gmail.com>
2025-03-29 22:34:57 +0600

    jasmon code

--- javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/moneyTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -8,4 +8,8 @@ describe("test suite: formateCurrency", () => {
   it("works with 0", () => {
     expect(formateCurrency(0)).toEqual("0.00");
   });
+
+  it("round up to the nearest cent", () => {
+    expect(formateCurrency(2095)).toEqual("20.95");
+  });
 });
