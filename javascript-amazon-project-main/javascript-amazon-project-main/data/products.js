87d7a241840b3cca0e0a02ffc95f08c268720f25
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-02 22:24:32 +0600

    this method

--- javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
@@ -73,6 +73,14 @@ function logThis() {
 logThis();
 logThis.call("hello");
 
+this;
+const object3 = {
+  method: () => {
+    console.log(this);
+  },
+};
+object3.method();
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
