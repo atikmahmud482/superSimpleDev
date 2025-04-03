3cdddf7f66f8c019425c3fe15d508491558adda2
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-03 22:42:15 +0600

    promise and callback

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
@@ -8,12 +9,27 @@ new Promise((resolve) => {
   loadProducts(() => {
     resolve();
   });
-}).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-});
+})
+  .then(() => {
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 
 /* loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
+}); */
+
+/* loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 }); */
