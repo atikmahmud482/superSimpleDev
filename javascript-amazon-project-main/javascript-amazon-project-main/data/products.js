8dfc00cb43d269c61f252727b5379376068ac823
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-03 22:00:29 +0600

    load data from the backend

--- javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
@@ -88,7 +88,13 @@ function loadProducts() {
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener("load", () => {
-    console.log(xhr.response);
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log(products);
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
