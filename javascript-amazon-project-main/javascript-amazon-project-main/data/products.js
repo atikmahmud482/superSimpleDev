66a5a3f85b1b3f1ea0fefb358762edfeb4f82267
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-05 22:31:44 +0600

    error handeling massage

--- javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
@@ -97,6 +97,9 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log("load products");
+    })
+    .catch((error) => {
+      console.log("unexpected error. Please try again later.");
     });
   return promise;
 }
@@ -119,11 +122,10 @@ export function loadProducts(fun) {
     fun();
   });
 
-  xhr.addEventListener("error", () => {
+  xhr.addEventListener("error", (error) => {
     console.log("unexpected error. Please try again later.");
   });
 
-  xhr.open("GET", "https://kdfvklj.supersimplebackend.dev/products");
+  xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
-loadProducts();
