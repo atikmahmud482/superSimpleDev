d172188228ae52a270f2dc93a8b0e40cae59fdd2
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-07 23:14:46 +0600

    number of lines added

--- Tech with tim/project.js
+++ Tech with tim/project.js
@@ -19,5 +19,18 @@ const deposit = () => {
     }
   }
 };
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("invalid number of lines, please try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
