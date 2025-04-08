ac81b6fdbdf75c7c9a3e1e4df3800cc6d72b30be
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-08 16:45:51 +0600

    Spin the slot machine

--- Tech with tim/project.js
+++ Tech with tim/project.js
@@ -72,11 +72,15 @@ const spin = () => {
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
       const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
     }
   }
+  return reels;
 };
 
-spin();
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
