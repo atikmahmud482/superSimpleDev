ad502d316b1c8720a29f77ee5aee4157a593593e
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-09 21:05:05 +0600

    project-1 adding value

--- Five Project/project-1.js
+++ Five Project/project-1.js
@@ -1,3 +1,11 @@
 const prompt = require("prompt-sync")();
 const name = prompt("what is your name? ");
-console.log(name);
+console.log("Hello", name, "Welcome to the game!");
+
+const shouldWePlay = prompt("Do you want to play? ");
+
+if (shouldWePlay.toLocaleLowerCase() === "yes") {
+  console.log("Great! Let's start the game!");
+} else if (shouldWePlay.toLocaleLowerCase() === "no") {
+  console.log("Okay, maybe next time!");
+}
