7d69a230071463b7292b3e6193d9d6f1333454fa
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-09 21:59:06 +0600

    project-1 adding value

--- Five Project/project-1.js
+++ Five Project/project-1.js
@@ -4,8 +4,26 @@ console.log("Hello", name, "Welcome to the game!");
 
 const shouldWePlay = prompt("Do you want to play? ");
 
-if (shouldWePlay.toLocaleLowerCase() === "yes") {
-  console.log("Great! Let's start the game!");
-} else if (shouldWePlay.toLocaleLowerCase() === "no") {
+if (shouldWePlay.toLowerCase() === "yes") {
+  // Game logic goes here
+  const lefOrRight = prompt(
+    "You enter a maze, do you want to go left or right? "
+  );
+  if (lefOrRight === "left") {
+    console.log("You will see the bridge...");
+    const cross = prompt("Do you want to cross the bridge? ");
+    if (cross === "yes") {
+      console.log(
+        "you cross but the bridge was weak and broke and you fall. you lost..."
+      );
+    } else {
+      console.log("Good choice...you win");
+    }
+  } else {
+    console.log("you will fall of a cliff...");
+  }
+} else if (shouldWePlay.toLowerCase() === "no") {
   console.log("Okay, maybe next time!");
+} else {
+  console.log("Please answer with 'yes' or 'no'. ");
 }
