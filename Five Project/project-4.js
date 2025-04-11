9264e20e024b633c27e9bdd610a44290afa57ac1
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-11 20:23:56 +0600

    project-4 checkWin board

--- Five Project/project-4.js
+++ Five Project/project-4.js
@@ -26,8 +26,53 @@ function printBoard(board) {
       if (j !== row.length - 1) rowString += " | ";
     }
     console.log(rowString);
-    if (i != board.length - 1) console.log("------------");
+    if (i != board.length - 1) console.log("----------");
   }
+}
+
+function checkWin(board) {
+  const lines = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ], // Row 1
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ], // Row 2
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ], // Row 3
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ], // Column 1
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ], // Column 2
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ], // Column 3
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ], // Diagonal \
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ], // Diagonal /
+  ];
 }
 
 const board = [
@@ -39,9 +84,13 @@ const board = [
 let turn = "X";
 let turnCount = 0;
 
+printBoard(board);
+console.log();
 while (turnCount < 9) {
   makeMove(turn, board);
   printBoard(board);
+  console.log();
+
   turn = turn === "X" ? "O" : "X";
   turnCount++;
 }
