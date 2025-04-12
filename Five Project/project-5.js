c39185e608c9829c149813705299e590bbdb22be
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-12 17:48:53 +0600

    project-5 showing json file on console

---
+++ Five Project/project-5.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync");
+const fs = require("fs");
+
+function loadQuestion() {
+  try {
+    const data = fs.readFileSync("project-5.json", "utf8");
+    const questions = JSON.parse(data).questions;
+    return questions;
+  } catch (e) {
+    console.log("Error occured loading file", e);
+    return [];
+  }
+}
+
+const questions = loadQuestion();
+console.log(questions);
