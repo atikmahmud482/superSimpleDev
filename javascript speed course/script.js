4b6460ed9a0bbea977e4ab1cdeb874b2da40c3cf
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-17 21:44:20 +0600

    arithmetic operations & type coercion

--- javascript speed course/script.js
+++ javascript speed course/script.js
@@ -12,9 +12,20 @@ number
 undefined
 null
 */
-var x = "hello";
+
+// variable
+/* var x = "hello";
 x = "world";
 
 let y = 5;
 
-const z = 3;
+const z = 3; */
+
+//Arithmetic Operators & Type Coercion
+const x = 5;
+const y = "5";
+
+console.log(y * x);
+console.log(y / x);
+console.log(y + x);
+console.log(y - x);
