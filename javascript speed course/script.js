755b32a9bf6a36ee0c184549535bd0c44ef74851
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-17 21:52:54 +0600

    Type Conversion

--- javascript speed course/script.js
+++ javascript speed course/script.js
@@ -22,10 +22,19 @@ let y = 5;
 const z = 3; */
 
 //Arithmetic Operators & Type Coercion
-const x = 5;
+/* const x = 5;
 const y = "5";
 
 console.log(y * x);
 console.log(y / x);
 console.log(y + x);
-console.log(y - x);
+console.log(y - x); */
+
+//Type Conversions
+// const x = "345";
+const x = "3445.455fld";
+const y = 3;
+
+console.log(Number(x) + y);
+console.log(parseInt(x) + y);
+console.log(parseFloat(x) + y);
