a0c4b50bce777be82e809cbae1e99b2ba281fe34
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-17 20:39:44 +0600

    data type

--- javascript speed course/script.js
+++ javascript speed course/script.js
@@ -1,4 +1,20 @@
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 
-const v = prompt("type something");
-console.log(v);
+// const v = prompt("type something");
+// console.log(v);
+
+/*
+ data types
+ ---------------- 
+string
+boolean
+number
+undefined
+null
+*/
+var x = "hello";
+x = "world";
+
+let y = 5;
+
+const z = 3;
