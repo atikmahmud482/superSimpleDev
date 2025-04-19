031ee1adbc16e82665f261f7085c2eee3ec7a3d2
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-19 22:43:24 +0600

    movie card components made

--- React project/front end/src/App.jsx
+++ React project/front end/src/App.jsx
@@ -3,20 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
+      {/* <div>
         <Text display="hi"></Text>
         <Text display="hello"></Text>
-      </div>
+      </div> */}
     </>
   );
 }
 
-function Text({ display }) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
-}
+// function Text({ display }) {
+//   return (
+//     <div>
+//       <p>{display}</p>
+//     </div>
+//   );
+// }
 
 export default App;
