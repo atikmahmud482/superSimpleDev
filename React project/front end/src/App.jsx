395009409a16966429aadaf56b4f9105ca6bda16
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-19 23:47:45 +0600

    adding navbar to linkup home page to other pages

--- React project/front end/src/App.jsx
+++ React project/front end/src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Favorite from "./pages/Favorites";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/favorite" element={<Favorite></Favorite>}></Route>
-      </Routes>
-    </main>
+    <div>
+      <Navbar></Navbar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/favorite" element={<Favorite></Favorite>}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
