136cfb155d215897d787fd153a18213f2b0600df
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-19 23:14:01 +0600

    search field and button workable

--- React project/front end/src/pages/Home.jsx
+++ React project/front end/src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "jhon wick", release_date: "2022" },
     { id: 1, title: "jhon wick", release_date: "2022" },
     { id: 1, title: "jhon wick", release_date: "2022" },
   ];
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    alert(searchQuery);
+  };
   return (
     <div className="home">
       <form onSubmit={handleSubmit} className="search-form">
@@ -13,6 +18,8 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
