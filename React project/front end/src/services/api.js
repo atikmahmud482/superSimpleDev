69c28f53875dc7950b5761fb47ba7eaabd95c6d1
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-20 22:40:35 +0600

    fetching data

---
+++ React project/front end/src/services/api.js
@@ -0,0 +1,8 @@
+const API_KEY = "57a513cdd59d3499a423208df6b78a13";
+const BASE_URL = "https//api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
