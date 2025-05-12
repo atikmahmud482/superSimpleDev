84b074386f2ded23dd4e9a17a72fd28f558808f7
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-12 20:47:01 +0600

    No-6 What is an array, and how to access its elements?

--- Javascript-interview/index.js
+++ Javascript-interview/index.js
@@ -26,3 +26,27 @@ const is a block and not reassign variable. */
 string, number, boolean, nul, undefined, symbol, BigInt.
 -Non-Primitive(3): data types are mutable.
 Function,array,object */
+
+/* // No-6: What is an array, and how to access its elements?
+An array or JavaScript is an ordered collection of values or elements, elements stored in the array can be of any data type but they all must belong to the same data type.
+
+In JavaScript, you can create an array either using the array literal syntax.
+let fruits = ["apple", "banana", "cherry"] 
+
+Or you can the road not taken with array constructor.
+let numbers = new Array(10, 20, 30);
+
+if you want to access a specific element in the array
+you can directly access them their indexes. 
+let fruits = ["apple", "banana", "cherry"] 
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+
+or, if you want to iterate through the whole array, then you can use a for-loop
+for(let i = 0; i< fruits.length; i++){
+    console.log(fruits[i])
+}
+
+or make it even simpler and use a for each loop 
+fruits.forEach(fruit => console.log(fruit)) */
