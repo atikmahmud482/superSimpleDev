3d318c8bef9bd3b25f9c1ec2acbbeea585c7441f
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-12 19:37:22 +0600

    No-1 & No-2 added

---
+++ Javascript-interview/index.js
@@ -0,0 +1,9 @@
+// No-1:  What is JavaScript and its common uses?
+/* javaScript is lightweight, interpreted scripting language used to build dynamic web content  */
+
+// No-2: What are template literals in JavaScript?
+/* let a = 10;
+let b = 34;
+console.log(`sum or ${a} and ${b} is ${a + b}`); */
+
+// No-3: What is hoisting? Provide an example.
