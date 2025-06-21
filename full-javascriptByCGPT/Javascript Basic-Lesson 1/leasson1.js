ecab189f669b76c17f256ee235e9e197e3a33f4c
atikmahmud482 <atikmahmud482@gmail.com>
2025-06-21 18:50:19 +0600

    Lesson 1: Variables and Data Types

---
+++ full-javascriptByCGPT/Javascript Basic-Lesson 1/leasson1.js
@@ -0,0 +1,20 @@
+// Lesson 1: Variables and Data Types
+
+const myName = "Atik Al Mahmud";
+const myAge = 25;
+const isLearningJavaScript = true;
+
+const favoriteThings = ["Coding", "Tea", "Problem Solving"];
+
+const myProfile = {
+  name: myName,
+  age: myAge,
+  favoriteSkill: "JavaScript",
+  location: "Bangladesh",
+};
+
+console.log("👤 Name:", myName);
+console.log("🎂 Age:", myAge);
+console.log("📘 Learning JS:", isLearningJavaScript);
+console.log("❤️ Favorite Things:", favoriteThings);
+console.log("📄 Profile Object:", myProfile);
