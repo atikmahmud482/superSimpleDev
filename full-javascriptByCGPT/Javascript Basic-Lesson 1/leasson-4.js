9c1066d180c742f6faa111a2d189780117c39db3
atikmahmud482 <atikmahmud482@gmail.com>
2025-06-23 18:35:44 +0600

    Lesson 4: What is Dom

---
+++ full-javascriptByCGPT/Javascript Basic-Lesson 1/leasson-4.js
@@ -0,0 +1,11 @@
+/* 🧪 JavaScript Lesson 4: DOM & Events
+✅ 1. What is the DOM?
+The DOM (Document Object Model) represents your HTML as a tree structure in JavaScript. You can use JS to:
+
+Select elements
+
+Change their content, style, or attributes
+
+Respond to user actions (clicks, typing, etc.)
+
+ */
