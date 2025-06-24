0ab9f4f7696100bb6a09cc8b95c2b2dbd93be542
atikmahmud482 <atikmahmud482@gmail.com>
2025-06-24 18:45:19 +0600

    Lesson 5: Array

---
+++ full-javascriptByCGPT/Javascript Basic-Lesson 1/leasson-5.js
@@ -0,0 +1,7 @@
+/* 🧪 JavaScript Lesson 5: Arrays, Objects & Array Methods
+✅ 1. Arrays
+An array holds multiple values in a single variable. */
+
+let skills = ["HTML", "CSS", "JavaScript", "Tailwind"];
+console.log(skills[0]); // "HTML"
+console.log(skills.length); // 4
