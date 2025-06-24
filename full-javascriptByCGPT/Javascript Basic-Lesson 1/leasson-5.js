31d77a50e9028b4b234f66cd035ea23fe4ca9643
atikmahmud482 <atikmahmud482@gmail.com>
2025-06-24 18:46:32 +0600

    Lesson 5: Objects

--- full-javascriptByCGPT/Javascript Basic-Lesson 1/leasson-5.js
+++ full-javascriptByCGPT/Javascript Basic-Lesson 1/leasson-5.js
@@ -5,3 +5,19 @@ An array holds multiple values in a single variable. */
 let skills = ["HTML", "CSS", "JavaScript", "Tailwind"];
 console.log(skills[0]); // "HTML"
 console.log(skills.length); // 4
+
+/* ✅ 2. Objects
+An object stores key–value pairs. */
+
+let user = {
+  name: "Atik",
+  age: 25,
+  isDeveloper: true,
+};
+
+console.log(user.name); // "Atik"
+console.log(user["age"]); // 25
+
+// You can also update or add values:
+user.age = 26;
+user.country = "Bangladesh";
